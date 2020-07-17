0f6921e8fa00f455aba53a21f66dcd9ccad75571
victorscholz <vgscholz@gmail.com>
2020-07-17 13:56:28 -0400

    finished, collaborated with Ward Price

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,20 @@
+require_relative './student.rb'
+
 class BoatingTest
+    attr_accessor :student, :test, :status, :instructor
+
+    @@all = []
+
+    def initialize(student, test, status, instructor)
+        @student = student
+        @test = test
+        @status = status
+        @instructor = instructor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,34 @@
-class Instructor
+require_relative './student.rb'
+require_relative './boatingtest.rb'
 
+class Instructor
+    attr_reader :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, boating_test)
+        if BoatingTest.all.include?(student) && BoatingTest.all.include?(boating_test)
+            test.status = "passed"
+        else 
+            BoatingTest.new(student, boating_test, status="passed", self)
+        end
+    end
+
+    def fail_student(student, boating_test)
+        if BoatingTest.all.include?(student) && BoatingTest.all.include?(boating_test)
+            test.status = "failed"
+        else 
+            BoatingTest.new(student, boating_test, status="failed", self)
+        end
+    end
 end
+
