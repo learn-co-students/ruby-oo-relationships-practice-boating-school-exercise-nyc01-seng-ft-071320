973c617274dd94281938014463720dc82a6d377d
sawandiwilson <Sawandiwilson@gmail.com>
2020-07-17 10:10:26 -0400

    student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Student
 
+    attr_accessor :status, :instructor, :name
+
+    @@all = []
+
+    def initialize(name, status)
+    @name = name
+    @status = status 
+    @instructor = instructor 
+    end
+
+    def add_instances 
+    @@all << self 
+    end
+binding.pry 
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,21 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+class Student
 
+  attr_accessor :status, :instructor, :name
+
+  @@all = []
+
+  def initialize(name, status)
+  @name = name
+  @status = status 
+  @instructor = instructor 
+  end
+
+  def add_instances 
+  @@all << self 
+  end
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
