10d73e61ff258d5d45601a9711da8592c2dfc651
David-Kirsch <davidjkirsch@gmail.com>
2020-07-17 11:26:58 -0400

    Completed Boating Lab

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,32 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+        BoatingTest.all.select do |test| 
+            if (test.student == student && test.test_name == test_name)
+                test.test_status = "passed"
+            else
+                student = BoatingTest.new(student, test_name, "passed", self)
+            end         
+        end
+    end
+
+    def fail_student(student, test_name)
+        BoatingTest.all.select do |test|
+            if(test.student == student && test.test_name == test_name)
+                test.test_status = "failed"
+            else
+                student = BoatingTest.new(student, test_name, "failed", self)
+            end
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
+require 'pry'
+require_relative "boatingtest.rb"
+require_relative "instructor.rb"
 class Student
+
+    attr_accessor :first_name
+    @@all = []
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        self.all.find do |student| student.first_name == first_name
+        end
+    end
+
+    def grade_percentage(first_name)
+        student_record = BoatingTest.all.select do |test| test.student.first_name == first_name end
+
+        test_passed = student_record.count do |test| test.test_status == "passed" end
+
+        percent = (test_passed.to_f / student_record.count.to_f) * 100
+    end
 
 end
