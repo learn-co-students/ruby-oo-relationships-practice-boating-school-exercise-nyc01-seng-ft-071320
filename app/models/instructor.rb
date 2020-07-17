267f01f23b0756568692157c88461acc9c6e32f1
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-17 13:17:50 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,10 +13,6 @@ class Instructor
         @@all
     end
  
-    # def find_test(student, test)
-    #     BoatingTest.find_by_student_and_name(student, test)
-    # end
-    
     def pass_student(student, test)
         if BoatingTest.find_test(student, test)
             BoatingTest.find_test(student, test).test_status = "passed"
